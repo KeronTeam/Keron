8a261cc9f53c898d9a78612fe24ab35db665cb0f
Grégoire Astruc <gregoire.astruc@gmail.com>
2015-03-04 23:31:46 +0100

    Target framework 2.0

--- premake5.lua
+++ premake5.lua
@@ -170,6 +170,7 @@ solution "Keron"
     project "ENet-net"
         kind "SharedLib"
         language "C#"
+        framework "2.0"
         targetname "ENet"
         targetdir(lib_outdir)
         files { "enetcs/ENetCS/**.cs" }

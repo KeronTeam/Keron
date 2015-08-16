e73365b7318764fff059de5cc6ab1ed91fd90094
Grégoire Astruc <gregoire.astruc@gmail.com>
2015-08-16 19:24:15 -0400

    Moved to container, use cache for tools

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
+PREFIX=$HOME
 PREMAKE_VERSION=5.0.0-alpha4-linux
 PREMAKE_URL=https://github.com/premake/premake-core/releases/download/v5.0.0.alpha4/premake-${PREMAKE_VERSION}.tar.gz
 CMAKE_VERSION=3.3.1-Linux-x86_64
 CMAKE_URL=http://www.cmake.org/files/v3.3/cmake-${CMAKE_VERSION}.tar.gz
 
-[ ! -d tools ] && mkdir -p tools/bin
+[ ! -d $PREFIX/tools ] && mkdir -p $PREFIX/tools/bin
 
 # Premake
 wget -O /tmp/premake-dev-linux.tar.gz $PREMAKE_URL
 ls -l /tmp/premake-dev-linux.tar.gz
 file /tmp/premake-dev-linux.tar.gz
-tar -C tools/bin -xzf /tmp/premake-dev-linux.tar.gz
+tar -C $PREFIX/tools/bin -xzf /tmp/premake-dev-linux.tar.gz
 
 # CMake
 wget -O /tmp/cmake-linux.tar.gz $CMAKE_URL
 ls -l /tmp/cmake-linux.tar.gz
 file /tmp/cmake-linux.tar.gz
-tar -C tools -xzf /tmp/cmake.tar.gz
-ln -s $PWD/cmake-$CMAKE_VERSION/bin/* $PWD/tools/bin
+tar -C $PREFIX/tools -xzf /tmp/cmake.tar.gz
+ln -s $PWD/cmake-$CMAKE_VERSION/bin/* $PREFIX/tools/bin
 
 # KSP runtime archive.
 wget --no-check-certificate -q -O /tmp/ksp-runtime-linux.7z $ARCHIVE_URL

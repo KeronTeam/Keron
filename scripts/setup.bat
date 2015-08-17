9832c779c50189117323b3ab2df6e9dc37c7eb40
Grégoire Astruc <gregoire.astruc@gmail.com>
2015-08-17 00:05:27 -0400

    Typo

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,5 +1,5 @@
 cinst -y curl
 cinst -v -f -y devbox-common.extension
 cinst -v -f -y devbox-sed
-url -sSL -o premake.zip https://github.com/premake/premake-core/releases/download/v5.0.0.alpha4/premake-5.0.0.alpha4-windows.zip
+curl -sSL -o premake.zip https://github.com/premake/premake-core/releases/download/v5.0.0.alpha4/premake-5.0.0.alpha4-windows.zip
 unzip premake.zip

5e8f2d280f54e03657b2684d982ebbb76a99df4b
Grégoire Astruc <gregoire.astruc@gmail.com>
2015-08-19 16:45:18 -0400

    windows CI: Don't install sed

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,5 +1,3 @@
 cinst -y curl
-cinst -v -f -y devbox-common.extension
-cinst -v -f -y devbox-sed
 curl -sSL -o premake.zip https://github.com/premake/premake-core/releases/download/v5.0.0.alpha4/premake-5.0.0.alpha4-windows.zip
 unzip premake.zip

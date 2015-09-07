a8dc35bdf184468cb0f9c9f08b4db6f5227144c8
Grégoire Astruc <gregoire.astruc@gmail.com>
2015-09-06 22:25:06 -0400

    appveyor: initial attempt for cmake

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,3 +1,3 @@
 cinst -y curl
-curl -sSL -o premake.zip https://github.com/premake/premake-core/releases/download/v5.0.0.alpha4/premake-5.0.0.alpha4-windows.zip
-unzip premake.zip
+curl -sSL --insecure -o ksp-runtime.7z %archive_url%
+7z x -p%archive_pwd% ksp-runtime.7z

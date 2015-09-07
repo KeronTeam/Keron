45ef945f1747750bcaf9fbfd5cc2daee66e250b0
Grégoire Astruc <gregoire.astruc@gmail.com>
2015-09-07 14:10:15 -0400

    appveyor: x32 builds for client

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -11,7 +11,11 @@ cd build
 rem select generator.
 set GENERATOR="Visual Studio %VS_VERSION%"
 if "%PLATFORM%"=="x64" set GENERATOR="Visual Studio %VS_VERSION% Win64"
+set CLIENT_ONLY=""
+
+rem the server only works for x64
+if "%PLATFORM%"=="Win32" set CLIENT_ONLY="-DKERON_BUILD_SERVER=OFF"
 
 rem configure build files
-cmake -G %GENERATOR% -DCMAKE_BUILD_TYPE=%CONFIGURATION% -DKSP_MANAGED_PATH=%cd%/../KSP_runtime/KSP_Data/Managed ..
+cmake -G %GENERATOR% -DCMAKE_BUILD_TYPE=%CONFIGURATION% %CLIENT_ONLY% -DKSP_MANAGED_PATH=%cd%/../KSP_runtime/KSP_Data/Managed ..
 cd ..
